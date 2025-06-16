ff1715285350cc3613334c53dd754ad5f95f6531
B Divya <divyareddy4804@gmail.com>
2025-06-16 22:10:20 +0530

    Add files via upload

---
+++ Basic Web Development/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("clickButton");
+  button.addEventListener("click", function () {
+    alert("Hello! You clicked the button.");
+  });
+});
